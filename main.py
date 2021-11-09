d73829f8b2dfd3037f2ee1944fb5f9418a21e7cc
Mohamed-Ibrahim-124 <mohamedibrahim_124@outlook.com>
2021-11-09 21:36:47 +0200

    Updated the extraction of number of cites and longest city name

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ if __name__ == '__main__':
     with open("countries.csv", 'r') as j:
         countries = json.loads(j.read())
         result = []
+        longest_city_name = ''
+        number_of_countries = 0
         for key in countries:
+            longest_city_name = max(max(countries[key]),(longest_city_name))
+            number_of_countries +=len(countries[key])
             result.append([key , len(countries[key]) , max(countries[key])])
+        print("longest City Name =",longest_city_name,"Number of Cities=",number_of_countries)
         header = ["Country Name","Number of cities","City with max length"]
         with open('result.csv', 'w', encoding='UTF8') as f:
             writer = csv.writer(f)
